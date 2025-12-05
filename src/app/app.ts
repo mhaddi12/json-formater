2447a542a383a448caa5ff2894ac3a9eff49400c
Haddi <se.haddi1@gmail.com>
2025-12-05 18:37:30 +0500

    C make the build script

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,36 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal,NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
+import { NgxJsonViewerModule } from 'ngx-json-viewer';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,NgxJsonViewerModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('formater');
+  userJson: string = '';
+  parsedJson: any = null;
+  error: string = '';
+  copySuccess: string = '';
+
+  parseJson() {
+    try {
+      this.parsedJson = JSON.parse(this.userJson);
+      this.error = '';
+    } catch (e) {
+      this.error = 'Invalid JSON!';
+      this.parsedJson = null;
+    }
+  }
+
+    copyJson() {
+    if (this.parsedJson) {
+      const formattedJson = JSON.stringify(this.parsedJson, null, 2);
+      navigator.clipboard.writeText(formattedJson).then(() => {
+        this.copySuccess = 'JSON copied to clipboard!';
+        setTimeout(() => (this.copySuccess = ''), 3000);
+      });
+    }
+  }
 }
